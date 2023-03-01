0a4123b9209a7736acf9e13748d09a7b9c16c520
Nur Fadilah Zulfi <nurfadilahzulfi@gmail.com>
2023-03-01 08:49:57 +0700

    commit kedua

---
+++ penjumlahan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main(){
+    int a,b,hasil;
+
+    printf("penjumlahan bilangan sederhana\n");
+
+    printf("input bilangan pertama :");
+    scanf("%d",&a);
+    printf("input bilangan kedua :");
+    scanf("%d",&b);
+
+    hasil=a+b;
+
+    printf("hasil penjumlahan %d + %d adalah : %d",a,b,hasil);
+}
